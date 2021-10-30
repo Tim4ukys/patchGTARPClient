65582169a23e0891be989e97c5e294d8e6765cf8
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2021-10-30 21:37:19 +0700

    fix bugs

--- source/patch.hpp
+++ source/patch.hpp
@@ -13,22 +13,29 @@
 
 // Win-API: VirtualProtect, typedef BOOL
 #include <Windows.h>
+// STL: std::stringstream
+#include <sstream>
+// STL: std:setfill, std::setw
+#include <iomanip>
 // C (Old-Library): strlen()
 #include <cstring>
 // C (Old-Library): uint8_t, uint16_t, uint32_t, uint64_t
 #include <cstdint>
 
-namespace patch 
+/*
+	В след. обновлении переделаю модуль под C язык, чтобы добиться максимальной скорости.
+*/
+namespace patch
 {
 	/*
 	* @breaf Конвертирует HEX строку в массив с байтами
 	* @param hex Строка в которой записаны числа в HEX формате
 	* @return Контейнер vector с байтами внутри
 	*/
-	inline std::vector<uint8_t> HEXtoRaw(std::string hex)
+	inline std::vector<uint8_t> HEXtoRaw(std::string hex) // vector::push_back - не есть хорошо.
 	{
 		std::vector<uint8_t> byte{};
-		
+
 		for (size_t i = 0; i + 1 < hex.size(); i += 2)
 		{
 			std::string h{}; h.push_back(hex[i]); h.push_back(hex[i + 1]);
@@ -43,7 +50,7 @@ namespace patch
 	* @param offsetAddress Адрес переменной
 	* @return Состояние проделанной работы
 	*/
-	inline BOOL setPushOffset(uint32_t address, uint32_t offsetAddress)
+	inline BOOL setPushOffset(uint32_t address, uint32_t offsetAddress) // unsafe. В след. обновлении переделаю
 	{
 		std::stringstream sstream{};
 		sstream << std::uppercase << std::hex << offsetAddress;
@@ -57,7 +64,7 @@ namespace patch
 		{
 			uint8_t* pAddr = reinterpret_cast<uint8_t*>(address);
 			*pAddr = 0x68;
-			for	(size_t i = 1; i < 5; i++)
+			for (size_t i = 1; i < 5; i++)
 			{
 				size_t vecPos = 4 - i;
 				*(pAddr + i) = raw.size() > vecPos ? raw[vecPos] : 0;
@@ -75,16 +82,16 @@ namespace patch
 	* @param size Размер строки(массива байт)
 	* @return HEX строка
 	*/
-	inline std::string getHEX(uint64_t address, size_t size)
+	inline std::string getHEX(uint32_t address, size_t size)
 	{
 		std::stringstream sstream{};
-		
+
 		DWORD oldProtect;
 		if (VirtualProtect(reinterpret_cast<LPVOID>(address), size, PAGE_EXECUTE_READWRITE, &oldProtect))
 		{
 			for (size_t i = 0; i < size; i++)
 			{
-				sstream << std::uppercase << std::hex << static_cast<int>(*(reinterpret_cast<uint8_t*>(address) + i));
+				sstream << std::setfill('0') << std::setw(2) << std::uppercase << std::hex << static_cast<int>(*(reinterpret_cast<uint8_t*>(address) + i));
 			}
 
 			VirtualProtect(reinterpret_cast<LPVOID>(address), size, oldProtect, NULL);
@@ -96,66 +103,95 @@ namespace patch
 	/*
 	* @breaf Меняет участок памяти согласно по массиву байтов в строке
 	* @param address Адрес участка памяти с которого будет начинаться запись
+	* @param raw Массив байт
+	* @param size Размер строки(массива байт)
+	* @return Состояние проделанной работы
+	*/
+	inline BOOL setRaw(uint32_t address, std::vector<uint8_t> raw, size_t size)
+	{
+		DWORD oldProtect;
+
+		if (VirtualProtect(reinterpret_cast<LPVOID>(address), size, PAGE_EXECUTE_READWRITE, &oldProtect))
+		{
+			for (size_t i = 0; i < size; i++) {
+				*(reinterpret_cast<uint8_t*>(address) + i) = raw[i];
+			}
+
+			VirtualProtect(reinterpret_cast<LPVOID>(address), size, oldProtect, NULL);
+			return TRUE;
+		}
+		return FALSE;
+	}
+	/*
+	* @breaf Меняет участок памяти согласно по массиву байтов в строке
+	* @param address Адрес участка памяти с которого будет начинаться запись
 	* @param raw Сам текст, из которового будет браться массив байт
 	* @param size Размер строки(массива байт)
 	* @return Состояние проделанной работы
 	*/
-	inline BOOL setRaw(uint64_t address, const char* raw, size_t size)
-    {
-        DWORD oldProtect;
+	inline BOOL setRaw(uint32_t address, const char* raw, size_t size)
+	{
+		DWORD oldProtect;
 
-        if (VirtualProtect(reinterpret_cast<LPVOID>(address), size, PAGE_EXECUTE_READWRITE, &oldProtect))
-        {
-            for (size_t i = 0; i < size; i++) {
-                *(reinterpret_cast<uint8_t*>(address) + i) = static_cast<uint8_t>(raw[i]);
-            }
+		if (VirtualProtect(reinterpret_cast<LPVOID>(address), size, PAGE_EXECUTE_READWRITE, &oldProtect))
+		{
+			for (size_t i = 0; i < size; i++) {
+				*(reinterpret_cast<uint8_t*>(address) + i) = static_cast<uint8_t>(raw[i]);
+			}
 
-            VirtualProtect(reinterpret_cast<LPVOID>(address), size, oldProtect, NULL);
-            return TRUE;
-        }
-        return FALSE;
-    }
-    /*
+			VirtualProtect(reinterpret_cast<LPVOID>(address), size, oldProtect, NULL);
+			return TRUE;
+		}
+		return FALSE;
+	}
+	/*
 	* @breaf Меняет участок памяти согласно по массиву байтов в строке
 	* @param address Адрес участка памяти с которого будет начинаться запись
 	* @param raw Сам текст, из которового будет браться массив байт
 	* @return Состояние проделанной работы
 	*/
-    inline BOOL setRaw(uint64_t address, std::string raw)
-    {
-        DWORD oldProtect;
-        if (VirtualProtect(reinterpret_cast<LPVOID>(address), raw.size(), PAGE_EXECUTE_READWRITE, &oldProtect))
-        {
-            for (size_t i = 0; i < raw.size(); i++) {
-                *(reinterpret_cast<uint8_t*>(address) + i) = static_cast<uint8_t>(raw.at(i));
-            }
+	inline BOOL setRaw(uint32_t address, std::string raw)
+	{
+		DWORD oldProtect;
+		if (VirtualProtect(reinterpret_cast<LPVOID>(address), raw.size(), PAGE_EXECUTE_READWRITE, &oldProtect))
+		{
+			for (size_t i = 0; i < raw.size(); i++) {
+				*(reinterpret_cast<uint8_t*>(address) + i) = static_cast<uint8_t>(raw.at(i));
+			}
 
-            VirtualProtect(reinterpret_cast<LPVOID>(address), raw.size(), oldProtect, NULL);
-            return TRUE;
-        }
-        return FALSE;
-    }
+			VirtualProtect(reinterpret_cast<LPVOID>(address), raw.size(), oldProtect, NULL);
+			return TRUE;
+		}
+		return FALSE;
+	}
 
-    /*
+	/*
 	* @breaf Меняет определённый участок памяти по значению
 	* @param address Адрес участка памяти в котором будет производиться перезапись
 	* @param raw Сам текст, из которового будет браться массив байт
 	* @return Состояние проделанной работы
 	*/
 	template<typename T>
-	inline BOOL set(uint64_t address, T value)
+	inline BOOL set(uint32_t address, T value)
 	{
-        DWORD oldProtect;
-        if (VirtualProtect(reinterpret_cast<PVOID>(address), sizeof(T), PAGE_EXECUTE_READWRITE, &oldProtect))
-        {
+		DWORD oldProtect;
+		if (VirtualProtect(reinterpret_cast<PVOID>(address), sizeof(T), PAGE_EXECUTE_READWRITE, &oldProtect))
+		{
 			*reinterpret_cast<T*>(address) = value;
-        	VirtualProtect(reinterpret_cast<PVOID>(address), sizeof(T), oldProtect, NULL);
-            return TRUE;
-        }
-        return FALSE;
+			VirtualProtect(reinterpret_cast<PVOID>(address), sizeof(T), oldProtect, NULL);
+			return TRUE;
+		}
+		return FALSE;
 	}
 
-	inline BOOL fill(uint64_t address, size_t size, uint32_t value)
+	/*
+	* @breaf Заполняет участок памяти одним значением
+	* @param address Адрес участка памяти в котором будет производиться перезапись
+	* @param size Размер(кол-во) памяти, которой будет присвоенно значение value
+	* @param value Значение которое будет присвоенно
+	* @return Состояние проделанной работы
+	*/
+	inline BOOL fill(uint32_t address, size_t size, uint32_t value)
 	{
 		DWORD oldProtect;
 		if (VirtualProtect(reinterpret_cast<PVOID>(address), size, PAGE_EXECUTE_READWRITE, &oldProtect))
