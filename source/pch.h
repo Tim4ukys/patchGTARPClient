dccfc9691422b9a6aeeb7eba71cadc11ea05fcf9
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-02-27 16:06:06 +0700

    add precompiled headers

---
+++ source/pch.h
@@ -0,0 +1,47 @@
+﻿/****************************************************
+*                                                   *
+*    Developer: Tim4ukys                            *
+*                                                   *
+*    email: tim4ukys.dev@yandex.ru                  *
+*    vk: vk.com/tim4ukys                            *
+*                                                   *
+*    License: GNU GPLv3                             *
+*                                                   *
+****************************************************/
+#ifndef PCH_H
+#define PCH_H
+
+// STL
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <array>
+#include <regex>
+
+
+
+// C (Old-Library)
+#include <cstring>
+#include <cstdint>
+
+
+// WIN-API
+#include <Windows.h>
+#include <Psapi.h>
+#include <memory.h>
+
+
+// BOOST
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/beast.hpp>
+
+
+// my library
+#include "patch.h" 
+#include "client.h"
+
+#endif //PCH_H
