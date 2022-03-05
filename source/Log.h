793181b5dab2eae29fb3e6767f5599afd17d6569
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-03-06 01:06:19 +0700

    add logo

--- source/Log.h
+++ source/Log.h
@@ -1,3 +1,13 @@
+/****************************************************
+*                                                   *
+*    Developer: Tim4ukys                            *
+*                                                   *
+*    email: tim4ukys.dev@yandex.ru                  *
+*    vk: vk.com/tim4ukys                            *
+*                                                   *
+*    License: GNU GPLv3                             *
+*                                                   *
+****************************************************/
 #pragma once
 class Log {
 public:
