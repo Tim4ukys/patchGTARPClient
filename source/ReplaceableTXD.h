f9f2454d9206f84d81cf331e3cbb76eadb2c5e00
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-03-06 23:36:49 +0700

    add patch files

---
+++ source/ReplaceableTXD.h
@@ -0,0 +1,15 @@
+/****************************************************
+*                                                   *
+*    Developer: Tim4ukys                            *
+*                                                   *
+*    email: tim4ukys.dev@yandex.ru                  *
+*    vk: vk.com/tim4ukys                            *
+*                                                   *
+*    License: GNU GPLv3                             *
+*                                                   *
+****************************************************/
+#pragma once
+class ReplaceableTXD {
+public:
+    static void Process();
+};
