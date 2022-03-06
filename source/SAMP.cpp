bba83ef69e7a8150736fcc7014522208b9dadc77
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-03-06 13:37:18 +0700

    add new SAMP class

---
+++ source/SAMP.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include "SAMP.h"
+
+SAMP::SAMP() {
+    initPointerList();
+}
+
+bool SAMP::isSAMPInit() {
+    if (!g_pNetGame) initPointerList();
+    return g_pNetGame != nullptr;
+}
+
+void SAMP::initPointerList() {
+    enum eSampOffs {
+        INFO,
+        CHAT,
+        INPUT,
+        FNC_ADDENTRY,
+        FNC_CMDRECT
+    };
+    const uint32_t sampOffsets[]{
+        /*sampInfo, sampChat, sampInput, fnAddEntry, fnCmdRect*/
+        0x026E8DC, 0x26E8C8, 0x026E8CC, 0x00067460, 0x0069000 // 037 r3-1
+    };
+
+    g_pNetGame = *(PVOID*)g_sampBase.getAddress(sampOffsets[INFO]);
+    g_pChat = *(PVOID*)g_sampBase.getAddress(sampOffsets[CHAT]);
+    g_pInput = *(PVOID*)g_sampBase.getAddress(sampOffsets[INPUT]);
+
+    g_fncAddEntry = reinterpret_cast<decltype(g_fncAddEntry)>(sampOffsets[FNC_ADDENTRY]);
+    g_fncAddCmdProc = reinterpret_cast<decltype(g_fncAddCmdProc)>(sampOffsets[FNC_CMDRECT]);
+}
+
+void SAMP::addChatMessage(D3DCOLOR color, const char* fmt, ...) {
+    if (!g_pNetGame || !g_pChat || !g_fncAddEntry) return;
+
+    const size_t maxMsgLenght = 145U;
+    char         buff[maxMsgLenght];
+
+    va_list arrg;
+    va_start(arrg, fmt);
+    vsprintf_s(buff, fmt, arrg);
+    va_end(arrg);
+
+    g_fncAddEntry(g_pChat, CHAT_TYPE_DEBUG, buff, nullptr, color, NULL);
+}
---
+++ source/SAMP.h
@@ -0,0 +1,44 @@
+/****************************************************
+*                                                   *
+*    Developer: Tim4ukys                            *
+*                                                   *
+*    email: tim4ukys.dev@yandex.ru                  *
+*    vk: vk.com/tim4ukys                            *
+*                                                   *
+*    License: GNU GPLv3                             *
+*                                                   *
+****************************************************/
+#pragma once
+
+#ifndef _D3D9_H_
+typedef DWORD D3DCOLOR;
+#endif
+
+typedef void(__cdecl* CMDPROC)(const char*);
+
+enum eChatMessageType : unsigned char {
+    CHAT_TYPE_NONE = 0,
+    CHAT_TYPE_CHAT = 2,
+    CHAT_TYPE_INFO = 4,
+    CHAT_TYPE_DEBUG = 8
+};
+
+class SAMP {
+public:
+    
+    SAMP();
+    //~SAMP();
+
+    bool isSAMPInit();
+
+    void addChatMessage(D3DCOLOR color, const char* fmt, ...);
+
+private:
+
+    void initPointerList();
+
+    PVOID g_pNetGame{}, g_pChat{}, g_pInput{};
+
+    void(__thiscall* g_fncAddEntry)(PVOID pChat, eChatMessageType eType, const char* szString, const char* szNick, D3DCOLOR dwTextColor, D3DCOLOR dwNickColor) = nullptr;
+    void(__thiscall* g_fncAddCmdProc)(PVOID pInput, const char* szCmdName, CMDPROC cmdHandler) = nullptr;
+};
