a3674d90afd5e962d05bde14acaeaad2d19d8db2
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-03-06 23:41:27 +0700

    update 'loadModule' and replace unsafe delete 'g_pLdrpDereferenceModule'

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -9,11 +9,24 @@
 *                                                   *
 ****************************************************/
 #include "pch.h"
-#include "snippets.h"
-#include "Log.h"
 
-//snippets::DynamicLibrary g_gtarpclient{ "gtarp_clientside.asi" };
-Log g_Log("!000patchGTARPClientByTim4ukys.log");
+const char SAMP_CMP[] = "E86D9A0A0083C41C85C0";
+const char GTARP_CMP[] = "5E5DC20400CCCCCCCCCC";
+
+#define CURRENT_VERSION "6.0.0"
+#define GITHUB_URL      "github.com/Tim4ukys/patchGTARPClient"
+
+// ----------------------------------------
+
+#include "OldHUD.h"
+#include "UnlockConect.h"
+#include "CustomFont.h"
+#include "WhiteID.h"
+#include "ReplaceableTXD.h"
+#include "DelCarTable.h"
+#include "SortScreenshot.h"
+
+// ----------------------------------------
 
 patch::callHook *g_pLdrpDereferenceModule = nullptr;
 PDWORD __fastcall loadModule(struct ldrrModuleDLL* a1, PVOID a2) {
@@ -24,16 +37,46 @@ PDWORD __fastcall loadModule(struct ldrrModuleDLL* a1, PVOID a2) {
         wchar_t* pPluginName; // +30h
     }* _a1 = (ldrrModuleDLL*)a1;
 
-    auto origFnc = reinterpret_cast<PDWORD(__fastcall*)(ldrrModuleDLL*, PVOID)>(g_pLdrpDereferenceModule->getOriginal());
+    static auto origFnc = reinterpret_cast<PDWORD(__fastcall*)(ldrrModuleDLL*, PVOID)>(g_pLdrpDereferenceModule->getOriginal());
 
-    //g_Log.Write("hModule: %p | name: %s", DWORD(a1), ConvertWideToANSI(_a1->pPluginName).c_str());
-    //MessageBoxA(NULL, "Stop", "patchTest", MB_OK | MB_ICONINFORMATION);
     if (!wcscmp(_a1->pPluginName, L"gtarp_clientside.asi"))
     {
-        //g_Log << "[loader]: gtarp_clientside.asi - injected. Start hooking.";
+        if (auto cmp = patch::getHEX(g_gtarpclientBase.getAddress(0xBABE), 10U); cmp != GTARP_CMP) {
+            MessageBoxW(
+                NULL,
+                L"ERROR: Эта версия плагина ещё не поддерживает эту версию клиента игры.\n\n"
+                L"Пожалуйста, дождитесь обновление патча и обновите его. Текущая версия: " CURRENT_VERSION L"\n"
+                L"GitHub: " GITHUB_URL,
+                L"!000patchGTARPClientByTim4ukys.ASI",
+                MB_ICONERROR
+            );
+            g_Log.Write("gtarp_cmp: %s", cmp.c_str());
+            TerminateProcess(GetCurrentProcess(), -1);
+        } 
+        else if (cmp = patch::getHEX(g_sampBase.getAddress(0xBABE), 10); cmp != SAMP_CMP) {
+            MessageBoxW(
+                NULL,
+                L"ERROR: Эта версия плагина ещё не поддерживает эту версию SAMP.\n\n"
+                L"Пожалуйста, дождитесь обновление патча и обновите его. Текущая версия: " CURRENT_VERSION L"\n"
+                L"GitHub: " GITHUB_URL,
+                L"!000patchGTARPClientByTim4ukys.ASI",
+                MB_ICONERROR);
+            g_Log.Write("samp_cmp: %s", cmp.c_str());
+            TerminateProcess(GetCurrentProcess(), -1);
+        } 
+        else
+            g_Log << R"(Version "gtarp_clientside.asi" and "samp.dll" supported!!)";
 
+        g_Log << "[loader]: gtarp_clientside.asi - injected. Start patching.";
 
-        g_Log << "[loader]: end hooking. Destroy 'LdrpDereferenceModule' hook.";
+#define PROCESS(a) {a::Process}
+        std::function<void()> cock[]{PROCESS(OldHUD), PROCESS(UnlockConect), PROCESS(CustomFont), PROCESS(WhiteID), PROCESS(ReplaceableTXD),
+                                     PROCESS(DelCarTable), PROCESS(SortScreenshot)};
+        for (const auto& fnc : cock)
+            fnc();
+#undef PROCESS
+
+        g_Log << "[loader]: end patching. Destroy 'LdrpDereferenceModule' hook.";
         g_pLdrpDereferenceModule->uninstallHook();
     }
 
@@ -44,6 +87,17 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH: 
     {
+        OSVERSIONINFOA vers;
+        ZeroMemory(&vers, sizeof(OSVERSIONINFOA));
+        vers.dwOSVersionInfoSize = sizeof(OSVERSIONINFOA);
+
+        if (GetVersionExA(&vers)) {
+            g_Log.Write("[PC INFO]: Windows version: %s %u.%u build: %u | PlatformID - %u", vers.szCSDVersion, vers.dwMajorVersion,
+                        vers.dwMinorVersion, vers.dwBuildNumber, vers.dwPlatformId);
+        }
+
+        // ------------
+
         auto handleNTDLL = GetModuleHandleA("ntdll.dll");
         auto nAddressLdrLoadDll = DWORD(GetProcAddress(handleNTDLL, "LdrLoadDll"));
 
@@ -55,7 +109,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
     }
         break;
     case DLL_PROCESS_DETACH:
-        delete g_pLdrpDereferenceModule;
+        SAFE_DELETE(g_pLdrpDereferenceModule);
         break;
     }
     return TRUE;
