0e7bcb1f6b8ce01884e08efb45862d09ccc71e0b
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-03-23 18:40:11 +0700

    fix FSignal

--- source/FSignal.cpp
+++ source/FSignal.cpp
@@ -1,2 +1,7 @@
 #include "pch.h"
 #include "FSignal.h"
+
+//template<typename T>
+//inline FSignal<T>::FSignal()
+//    : m_lock() {
+//}
--- source/FSignal.h
+++ source/FSignal.h
@@ -2,6 +2,7 @@
 template<typename T>
 class FSignal : public std::vector<std::function<T>> {
 public:
+    //FSignal();
     inline FSignal& operator+=(std::function<T> func) {
         std::lock_guard<std::mutex> lock(m_lock);
         this->push_back(func);
